abe8c2d8ecd12c60447b99ac6a7077bed5f9829c
Sohom Pal <44247372+SohomPal@users.noreply.github.com>
2024-11-09 21:40:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -93,5 +93,5 @@ def get_ips():
 #         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=True)
